988f5e148ba66a8ceba2c1d2fdac0d442ba07300
innocurve <hjpw96@naver.com>
2025-02-05 02:43:48 +0900

    asd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
+// .eslintrc.js (FlatCompat 사용 버전)
 module.exports = (() => {
   const { FlatCompat } = require('@eslint/eslintrc');
   const compat = new FlatCompat({
     baseDirectory: __dirname,
   });
-  // next/core-web-vitals 확장만 사용합니다.
   return [
     ...compat.extends('next/core-web-vitals'),
   ];
